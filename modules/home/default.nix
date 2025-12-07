b0423fdcf6e443ee55de68efc5265b484efb167e
Mathipe98 <56786723+Mathipe98@users.noreply.github.com>
2025-12-07 20:27:42 +0100

    Omitted nixvim, ghostty

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,15 +1,15 @@
 { ... }:
 
 {
-  imports = [
+  limports = [
     ./terminals/tmux.nix
-    ./terminals/ghostty.nix
-    ./editors/nixvim.nix
+    #./terminals/ghostty.nix
+    #./editors/nixvim.nix
     ./cli/bat.nix
     ./cli/btop.nix
-    ./cli/bottom.nix
-    ./cli/eza.nix
-    ./cli/fzf.nix
+    # ./cli/bottom.nix
+    # ./cli/eza.nix
+    # ./cli/fzf.nix
     ./cli/git.nix
     ./cli/htop.nix
     ./cli/tealdeer.nix
@@ -18,4 +18,3 @@
   ];
 
 }
-
